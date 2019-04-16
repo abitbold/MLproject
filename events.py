70a6f27649b7041382b46eecc9c224b53924519b
abitbold <33513246+abitbold@users.noreply.github.com>
2019-04-16 16:22:49 -0400

    Update and rename holidays.py to events.py

--- events.py
+++ events.py
@@ -12,3 +12,9 @@ def get_market_holidays(start='2000-01-01', end='2019-01-01'):
     holidays = set(busdays).difference(opens)
     df = pd.DataFrame(pd.to_datetime(list(holidays)).sort_values())
     return df
+
+def get_fridays(start='2000-01-01', end='2019-01-01'):
+    weekmask = [0, 0, 0, 0, 1, 0, 0]
+    fridays = np.arange('2000-01-01', '2019-01-01', dtype='datetime64[D]')
+    fridays = pd.to_datetime(fridays[np.is_busday(fridays, weekmask=weekmask)])
+    return fridays
