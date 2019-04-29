47875bff6fec765d1fb207c11cc81edcf4c8903c
abitbold <33513246+abitbold@users.noreply.github.com>
2019-04-29 13:29:23 -0400

    Update percent.py

--- percent.py
+++ percent.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd
 import pandas_datareader as web
 import datetime as dt
+import events as eve
 
 ### Generate first table percentage of seasonality
 dax = web.get_data_yahoo('^GDAXI', start='2000-01-01', end='2010-12-31')
 dax.columns = ['High', 'Low', 'Open', 'Close', 'Volume', 'Adj']
-fridays = get_fridays()
-firsts = get_first_days()
-hol = get_market_holidays()
+fridays = eve.get_fridays()
+firsts = eve.get_first_days()
+hol = eve.get_market_holidays()
 daxR = dax.Adj.values[1:]/dax.Adj.values[0:-1]-1
 alldays = (daxR>0).mean()
 rfri = []
@@ -18,20 +19,19 @@ rfirsts = []
 
 for d in fridays:
     try:
-        rfri.append(dax.loc[d,'Adj']/dax.loc[pd.to_datetime(d+dt.timedelta(3)), 'Open']-1)
+        rfri.append(dax.loc[d+dt.timedelta(3),'Close']/dax.loc[pd.to_datetime(d+dt.timedelta(-1)), 'Close']-1)
     except:
         pass
 
 for d in firsts:
     try:
-        rfirsts.append(dax.loc[np.busday_offset((d+dt.timedelta(-7)).date(), 0, roll='backward'),'Adj']/dax.loc[np.busday_offset((d+dt.timedelta(+5)).date(), 0, roll='forward'),'Adj']-1)
-    
+        rfirsts.append(dax.loc[np.busday_offset((d+dt.timedelta(+5)).date(), 0, roll='backward'),'Close']/dax.loc[np.busday_offset((d+dt.timedelta(-7)).date(), 0, roll='forward'),'Close']-1)
     except:
         pass 
 
 for d in hol:
     try:
-        rhol.append(dax.loc[np.busday_offset((d+dt.timedelta(-1)).date(), 0, roll='backward'),'Adj']/dax.loc[np.busday_offset((d+dt.timedelta(2)).date(), 0, roll='forward'),'Adj']-1)
+        rhol.append(dax.loc[np.busday_offset((d+dt.timedelta(2)).date(), 0, roll='forward'),'Close']/dax.loc[np.busday_offset((d+dt.timedelta(-1)).date(), 0, roll='backward'),'Close']-1)
             
     except:
         pass 
@@ -39,3 +39,7 @@ for d in hol:
 rfri = np.array(rfri)
 rhol = np.array(rhol)
 rfirsts=np.array(rfirsts) 
+print((rfri>0).mean())
+print((rhol>0).mean())
+print((rfirsts>0).mean())
+print((daxR>0).mean())
